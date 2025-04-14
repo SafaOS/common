4f3f2b81a5436f301c956285a53f60872ae21c89
ObserverUnit <116169309+ObserverUnit@users.noreply.github.com>
2025-04-14 12:36:28 +0200

    [get-rustc.sh]: use correct rustc toolchain when already installed

--- get-rustc.sh
+++ get-rustc.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # This script echoes the rustc toolchain required based on `.latest_stable_rustc_version.lock`
+CURRENT_RUSTC_VERSION=$(rustc --version | cut -d' ' -f2)
 STABLE_RUSTC_VERSION=$(rustc +stable --version | cut -d' ' -f2)
 LAST_STABLE_RUSTC_VERSION=$(cat .latest_stable_rustc_version.lock)
 
-if [ $STABLE_RUSTC_VERSION == $LAST_STABLE_RUSTC_VERSION ]; then 
+if [ $CURRENT_RUSTC_VERSION == $LAST_STABLE_RUSTC_VERSION ]; then
+    SPECIFIER=""
+elif [ $STABLE_RUSTC_VERSION == $LAST_STABLE_RUSTC_VERSION ]; then 
     SPECIFIER="+stable"
 else 
     SPECIFIER="+$LAST_STABLE_RUSTC_VERSION"
